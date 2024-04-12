faf3fdf3d4dc20b7e659f00b68e54d7d4897cbc3
Sehajdeep Singh <114660532+SehajDeepSingh26@users.noreply.github.com>
2024-04-12 19:16:03 +0530

    added best.pt model of WD

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def main():
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
 
-    model = YOLO("yolov8l.pt")
+    model = YOLO("best.pt")
 
     box_annotator = sv.BoxAnnotator(
         thickness=2,
